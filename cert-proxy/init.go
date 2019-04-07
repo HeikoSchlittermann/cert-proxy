7398926885d011ebf974c1f232191b851487317c
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2019-04-07 22:41:28 +0200

    Deliver certificate

--- cert-proxy/init.go
+++ cert-proxy/init.go
@@ -1,11 +1,14 @@
 package main
 
-import "flag"
+import (
+	"flag"
+)
 
 func init() {
-	flag.StringVar(&crtFile, "crt", "crt.pem", "certificate file")
-	flag.StringVar(&keyFile, "key", "key.pem", "key file")
-    flag.StringVar(&caFile, "ca", "ca.pem", "CA file")
-	flag.StringVar(&serve, "serve", ":4433", "[host]:port for listener")
+	flag.StringVar(&opt.CrtFile, "crt", "crt.pem", "certificate file")
+	flag.StringVar(&opt.KeyFile, "key", "key.pem", "key file")
+	flag.StringVar(&opt.CAFile, "ca", "ca.pem", "CA file")
+	flag.StringVar(&opt.Serve, "serve", ":4433", "[host]:port for listener")
+	flag.StringVar(&opt.Certbase, "certbase", "/var/lib/dehydrated/certs", "cert base dir")
 	flag.Parse()
 }
--- cert-proxy/main.go
+++ cert-proxy/main.go
@@ -4,30 +4,59 @@ import (
 	. "cert-proxy/shared"
 	"crypto/tls"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 )
 
-var (
-	caFile, crtFile, keyFile string
-	serve                    string
-)
+var opt struct { // see init.go for defaults
+	Certbase                 string
+	CAFile, CrtFile, KeyFile string
+	Serve                    string
+}
 
 func serveWelcome(w http.ResponseWriter, r *http.Request) {
-	state := r.TLS
-	fmt.Fprintf(w, "Welcome %s\r\n",
-        state.PeerCertificates[0].Subject.CommonName)
+	cn := r.TLS.PeerCertificates[0].Subject.CommonName
+	fmt.Fprintln(w, "HALLO: ", cn, r.URL.Path)
+}
+
+func serveCrt(w http.ResponseWriter, r *http.Request) {
+
+	if strings.Contains(r.URL.Path, "..") {
+		http.Error(w, "No `..` allowed.", http.StatusNotAcceptable)
+		return
+	}
+
+	dir := http.Dir(opt.Certbase)
+	fn := filepath.Join(strings.TrimPrefix(r.URL.Path, "/cert/"), "cert.pem")
+	file, err := dir.Open(fn)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	io.Copy(w, file)
 }
 
 func main() {
 
+	http.HandleFunc("/cert/", serveCrt)
 	http.HandleFunc("/", serveWelcome)
 
 	// The certificate we present to the client
-	cert, err := tls.LoadX509KeyPair(crtFile, keyFile)
-	Check(err)
+	cert, err := tls.LoadX509KeyPair(opt.CrtFile, opt.KeyFile)
+	if err != nil {
+        log.Fatal(err)
+    }
+    pool, err := CertPool(opt.CAFile)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	listener, err := tls.Listen("tcp", serve, &tls.Config{
-		ClientCAs:    CertPool(caFile),
+	listener, err := tls.Listen("tcp", opt.Serve, &tls.Config{
+		ClientCAs:    pool,
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 		Certificates: []tls.Certificate{cert},
 	})
