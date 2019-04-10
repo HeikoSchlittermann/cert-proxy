6f80bd5f674c8b190403ce4c442c97cf5e668709
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2019-04-10 17:23:55 +0200

    Deliver private key (if authorized)

--- cert-proxy/init.go
+++ cert-proxy/init.go
@@ -10,5 +10,6 @@ func init() {
 	flag.StringVar(&opt.CAFile, "ca", "ca.pem", "CA file")
 	flag.StringVar(&opt.Serve, "serve", ":4433", "[host]:port for listener")
 	flag.StringVar(&opt.Certbase, "certbase", "/var/lib/dehydrated/certs", "cert base dir")
+	flag.StringVar(&opt.ClientConfigDir, "ccd", "clients", "client configuration dir")
 	flag.Parse()
 }
--- cert-proxy/main.go
+++ cert-proxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	. "cert-proxy/shared"
 	"crypto/tls"
 	"fmt"
@@ -15,24 +16,31 @@ var opt struct { // see init.go for defaults
 	Certbase                 string
 	CAFile, CrtFile, KeyFile string
 	Serve                    string
+	ClientConfigDir          string
 }
 
 func serveWelcome(w http.ResponseWriter, r *http.Request) {
 	cn := r.TLS.PeerCertificates[0].Subject.CommonName
-	fmt.Fprintln(w, "HALLO: ", cn, r.URL.Path)
+	fmt.Fprintln(w, "Welcome", cn, r.URL.Path)
 }
 
+// servePublic delivers public files
 func servePublic(w http.ResponseWriter, r *http.Request) {
 
+	// do not allow ..
 	if strings.Contains(r.URL.Path, "..") {
 		http.Error(w, "No `..` allowed.", http.StatusNotAcceptable)
 		return
 	}
 
-    fn := func() string {
-        parts := strings.Split(r.URL.Path, "/") // "" / "cert" / "domain"
-        return filepath.Join(parts[2], parts[1] + ".pem")
-    }()
+	// construct the filename, hide short-lived variables inside the
+	// path: "" / cert /example.com
+	// parts 0    1    2
+	var fn string
+	{
+		parts := strings.Split(r.URL.Path, "/")
+		fn = filepath.Join(parts[2], parts[1]+".pem")
+	}
 
 	file, err := http.Dir(opt.Certbase).Open(fn)
 	if err != nil {
@@ -43,26 +51,81 @@ func servePublic(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, file)
 }
 
+func servePrivate(w http.ResponseWriter, r *http.Request) {
+	if strings.Contains(r.URL.Path, "..") {
+		http.Error(w, "No `..` allowed.", http.StatusNotAcceptable)
+		return
+	}
+
+    domain, filename := func() (string, string) {
+			parts := strings.Split(r.URL.Path, "/")
+            return parts[2], filepath.Join(parts[2], parts[1]+".pem")
+    }()
+
+	cn := r.TLS.PeerCertificates[0].Subject.CommonName
+
+	// we have a per cn config file in <config-dir>/cn.conf
+	config, err := http.Dir(opt.ClientConfigDir).Open(cn)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Your cn "+cn+" is unknown", http.StatusForbidden)
+		return
+	}
+    defer config.Close()
+
+	// now check, if the current cn is allowed to access the domain,
+	// that is, we check, if the config file (already opened) contains
+	// a line with the current domain
+	ok := func() bool {
+		scanner := bufio.NewScanner(config)
+		for scanner.Scan() {
+			line := strings.Trim(strings.SplitN(scanner.Text(), "#", 2)[0], " \t\r")
+			if line == domain {
+				return true
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		return false
+	}()
+
+    if !ok {
+        log.Printf("%s is not authorized for %s\n", cn, domain)
+        http.Error(w, "You are not authorized", http.StatusForbidden)
+        return
+    }
+
+    file, err := http.Dir(opt.Certbase).Open(filename)
+    if err != nil {
+        log.Println(err)
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+    io.Copy(w, file)
+
+}
+
 func main() {
 
-	http.HandleFunc("/fullchain/", servePublic)
-	http.HandleFunc("/chain/", servePublic)
 	http.HandleFunc("/cert/", servePublic)
-	http.HandleFunc("/", serveWelcome)
+	http.HandleFunc("/chain/", servePublic)
+	http.HandleFunc("/fullchain/", servePublic)
+	http.HandleFunc("/privkey/", servePrivate)
 
 	// The certificate we present to the client
 	cert, err := tls.LoadX509KeyPair(opt.CrtFile, opt.KeyFile)
 	if err != nil {
-        log.Fatal(err)
-    }
-    pool, err := CertPool(opt.CAFile)
-    if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
+	pool, err := CertPool(opt.CAFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	listener, err := tls.Listen("tcp", opt.Serve, &tls.Config{
 		ClientCAs:    pool,
-		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientAuth:   tls.RequireAndVerifyClientCert, // !! RequireAndVerify !!
 		Certificates: []tls.Certificate{cert},
 	})
 	Check(err)
