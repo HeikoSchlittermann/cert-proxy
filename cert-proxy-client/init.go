791a191a6d191e24fa8b55b81aeed9b710be511f
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2019-04-11 12:02:44 +0200

    Reorder the options and make the explanation more useful

--- cert-proxy-client/init.go
+++ cert-proxy-client/init.go
@@ -16,9 +16,9 @@ func init() {
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&opt.SSLFile, "auth", "proxy.pem", "proxy auth file (crt+key+ca) PEM")
-	flag.StringVar(&opt.Connect, "connect", "localhost:4433", "address of cert proxy server")
-	flag.StringVar(&opt.ServerCN, "server name", "cert-proxy", "CN of the expected proxy server certificate")
+	flag.StringVar(&opt.SSLFile, "authfile", "proxy.pem", "proxy auth file (crt+key+ca) PEM")
+	flag.StringVar(&opt.Connect, "cert-proxy", "localhost:4433", "address of cert proxy server")
+	flag.StringVar(&opt.ServerCN, "cert-proxy-cn", "cert-proxy", "CN of the cert proxy certificate")
 	flag.StringVar(&opt.Certbase, "certbase", CERTBASE, "base dir for downloaded certs")
 	flag.StringVar(&opt.Outfile, "outfile", "", "output file (use - for stdout)")
 	flag.BoolVar(&opt.Verbose, "verbose", false, "verbose output")
