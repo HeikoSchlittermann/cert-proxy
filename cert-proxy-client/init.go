99007ed9298bdbc5c1dae42955eb6dab0efab8ad
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2019-04-19 20:29:17 +0200

    Reorder the options

--- cert-proxy-client/init.go
+++ cert-proxy-client/init.go
@@ -21,14 +21,14 @@ func init() {
 	// -outformat PEM    implies cert, chain, fullchain, privkey
 	// -outformat PKCS12 implies bundle
 
-	flag.StringVar(&opt.SSLFile, "sslfile", "ssl.pem", "SSL auth file (crt+key+ca) PEM")
-	flag.StringVar(&opt.Connect, "connect", "https://localhost:4433", "Address of cert proxy server")
-	flag.StringVar(&opt.ServerCN, "cert-proxy-cn", "cert-proxy", "CN of the cert proxy certificate")
-	flag.StringVar(&opt.Certbase, "certbase", "certs", "Base dir for downloaded certs")
-	flag.StringVar(&opt.Outfile, "outfile", "", "Output file (use - for stdout)")
-	flag.StringVar(&opt.CNfile, "cnfile", "", "CN list file (use - for stdin)")
 	flag.BoolVar(&opt.Verbose, "verbose", false, "Verbose output")
 	flag.IntVar(&opt.Jobs, "jobs", 3, "Number of parallel running jobs")
+	flag.StringVar(&opt.CNfile, "cnfile", "", "CN list file (use - for stdin)")
+	flag.StringVar(&opt.Certbase, "certbase", "certs", "Base dir for downloaded certs")
+	flag.StringVar(&opt.Connect, "connect", "https://localhost:4433", "Address of cert proxy server")
+	flag.StringVar(&opt.Outfile, "outfile", "", "Output file (use - for stdout)")
+	flag.StringVar(&opt.SSLFile, "sslfile", "ssl.pem", "SSL auth file (crt+key+ca) PEM")
+	flag.StringVar(&opt.ServerCN, "cert-proxy-cn", "cert-proxy", "CN of the cert proxy certificate")
 	flag.Var(&opt.OutFormat, "format", "Format of the requested certificate(s)")
 	flag.Parse()
 
