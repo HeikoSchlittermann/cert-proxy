d7a263a8c63fece630cb18f55e50e6843b6bf802
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2019-09-09 23:11:36 +0200

    Add testing for secret/

---
+++ cmd/cert-proxy-client/secret/secret_test.go
@@ -0,0 +1,36 @@
+package secret
+
+import (
+	"os"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+
+	// String
+	t.Log("PASS")
+	if pass, err := Read("PASS:foo"); err != nil {
+		t.Errorf("unexpected: %v\n", err)
+	} else if pass != "foo" {
+		t.Errorf("expected %q, got %q\n", "foo", pass)
+	}
+
+	// Environment
+	t.Log("ENV")
+	os.Setenv("PW", "bar")
+	if pass, err := Read("ENV:PW"); err != nil {
+		t.Errorf("unexpected: %v\n", err)
+	} else if pass != "bar" {
+		t.Errorf("expected %q, got %q\n", "bar", pass)
+	}
+
+	// File
+	t.Log("FILE")
+	if pass, err := Read("FILE:pwfile"); err != nil {
+		t.Errorf("unexpected: %v\n", err)
+	} else if pass != "baz" {
+		t.Errorf("expected %q, got %q\n", "baz", pass)
+	}
+
+
+}
