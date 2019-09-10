d803ae7da9bc04cae70e162057d76ff75bdc5769
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2019-09-10 09:53:40 +0200

    Disable the version check warning
    FIXME: This should be replaced by a API version check, or *iff* we
    compare versions, we should focus on major.minor (semver)

--- cmd/cert-proxy-server/version.go
+++ cmd/cert-proxy-server/version.go
@@ -7,9 +7,11 @@ import (
 )
 
 func versionCheck(w http.ResponseWriter, req *http.Request) {
+	/* FIXME: Do not compare the versions, but establish
 	if program.Version != req.Header.Get(`x-version`) {
 		log.Printf("WARNING: Version mismatch: server:%s client:%s",
 			program.Version, req.Header.Get(`x-version`))
 	}
+	*/
 	w.Header().Add(`x-version`, program.Version)
 }
