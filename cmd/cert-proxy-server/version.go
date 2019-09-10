HeikoSchlittermann/cert-proxy

commit d803ae7da9bc04cae70e162057d76ff75bdc5769

Disable the version check warning
FIXME: This should be replaced by a API version check, or *iff* we
compare versions, we should focus on major.minor (semver)

cmd/cert-proxy-server/version.go
@@ -7,9 +7,11 @@ import (
 )
 
 func versionCheck(w http.ResponseWriter, req *http.Request) {
+	/* FIXME: Do not compare the versions, but establish
 	if program.Version != req.Header.Get(`x-version`) {
 		log.Printf("WARNING: Version mismatch: server:%s client:%s",
 			program.Version, req.Header.Get(`x-version`))
 	}
+	*/
 	w.Header().Add(`x-version`, program.Version)
 }

commit e04978e35dfa376e730a61e77b3f654e68f5f40b

Fix build, broken by d803ae7da9bc04cae70e162057d76ff75bdc5769

cmd/cert-proxy-server/version.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"cert-proxy/internal/program"
-	"log"
 	"net/http"
 )
 