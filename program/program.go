bbf6ae54b71aa4b6b1330aae5f84f855fc2faa68
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2021-09-08 18:22:04 +0200

    Fix missing version string

--- program/program.go
+++ program/program.go
@@ -6,13 +6,13 @@ import (
 )
 
 var (
-    Version string = `*unknown*`
-    Name string = filepath.Base(os.Args[0])
-    Path string = func() string {
-        if p, err := filepath.Abs(os.Args[0]); err != nil {
-            panic(err)
-        } else {
-            return p
-        }
-    }()
+	Version string = `*unknown*` // overriden by the linker
+	Name    string = filepath.Base(os.Args[0])
+	Path    string = func() string {
+		if p, err := filepath.Abs(os.Args[0]); err != nil {
+			panic(err)
+		} else {
+			return p
+		}
+	}()
 )
