801247c871fab5bac59387b1300304718c2e85b8
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2026-05-24 18:29:38 +0200

    fix: version

--- internal/program/program.go
+++ internal/program/program.go
@@ -7,19 +7,26 @@ package program
 import (
 	"os"
 	"path/filepath"
+	"runtime/debug"
 )
 
 var (
 	// Version is the version of the current program, as set by the
 	// builder (linker)
-	Version = `*unknown*` // overridden by the linker
+	Version = func() string {
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			panic("can't read built-in build info")
+		}
+		return bi.Main.Version
+	}()
 
 	// Name is the basename of the current process
 	Name = filepath.Base(os.Args[0])
 
 	// Path is the absolute path of the running executable.
 	Path = func() string {
-		p, err := filepath.Abs(os.Args[0])
+		p, err := os.Executable()
 		if err != nil {
 			panic(err)
 		}
